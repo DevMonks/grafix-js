bc70f4ed264aacb85154f040185f41b96b47016b
DevMonksTorben <theunichi@devmonks.net>
2013-07-22 13:00:40 +0200

    KeySpline has been merged into Utils

--- js/grafix/Utils.js
+++ js/grafix/Utils.js
@@ -1,87 +1,131 @@
 var Utils = {
-
-    merge: function ( target, source ) {
-        for ( var i in source ) {
+    
+    merge: function( target, source ) {
+        
+        for( var i in source )
             target[ i ] = source[ i ];
-        }
 
         return target;
     },
 
-    extend: function ( parent, childMethods ) {
+    extend: function( parent, childMethods ) {
+
         var instance = new parent();
-        for ( var i in childMethods ) {
+        for( var i in childMethods ) {
+            
             var desc = Object.getOwnPropertyDescriptor( childMethods, i );
             Object.defineProperty( instance, i, desc );
         }
-
+        
         return instance;
     },
 
-    isType: function ( val, type ) {
+    isType: function( val, type ) {
+
         return typeof val === type;
     },
 
-    isString: function ( val ) {
-        return Utils.isType( val, 'string' );
+    isString: function( val ) {
+
+        return this.isType( val, 'string' );
     },
 
-    isArray: function ( val ) {
+    isArray: function( val ) {
+
         return Array.isArray( val );
     },
 
-    isObject: function ( val ) {
-        return Utils.isType( val, 'object' );
+    isObject: function( val ) {
+
+        return this.isType( val, 'object' );
     },
 
-    isNumber: function ( val ) {
-        return Utils.isType( val, 'number' );
+    isNumber: function( val ) {
+
+        return this.isType( val, 'number' );
     },
 
-    isNumeric: function ( val ) {
+    isNumeric: function( val ) {
+
         return !isNaN( parseFloat( val ) ) && isFinite( val );
     },
 
-    isFunction: function ( val ) {
-        return Utils.isType( val, 'function' );
+    isFunction: function( val ) {
+        
+        return this.isType( val, 'function' );
     },
 
-    isDomNode: function ( o ) {
-        return o && (
-            Utils.isObject( Node ) ?
-                (o instanceof Node) :
-                Utils.isObject( o ) && Utils.isNumber( o.nodeType ) && Utils.isString( o.nodeName )
-            );
-    },
+    inArray: function( array, value ) {
 
-    isDomElement: function ( o ) {
-        return o && (
-            Utils.isObject( HTMLElement ) ?
-                (o instanceof HTMLElement) :
-                Utils.isObject( o ) && o.nodeType === 1 && Utils.isString( o.nodeName )
-            );
-    },
-
-    inArray: function ( array, value ) {
-        if ( array.indexOf ) {
+        if( array.indexOf )
             return array.indexOf( value ) !== -1;
-        }
 
-        for ( var i = 0; i < array.length; ) {
-            if ( array[i] === value ) {
+        for( var i in array )
+            if( array[ i ] === value )
                 return true;
-            }
-        }
 
         return false;
     },
 
-    decToRad: function ( dec ) {
+    decToRad: function( dec ) {
+
         return dec * Math.PI / 180;
     },
+            
+    radToDec: function( rad ) {
 
-    radToDec: function ( rad ) {
         return ( rad * 180 ) / Math.PI;
-    }
+    },
 
+    keySpline: function( x1, y1, x2, y2, epsilon ) {
+
+        var epsilon = epsilon || 0.002; //default precision
+
+        var b1 = function( t ) { return t * t * t; };
+        var b2 = function( t ) { return 3 * t * t * ( 1 - t ); };
+        var b3 = function( t ) { return 3 * t * ( 1 - t ) * ( 1 - t ); };
+        var b4 = function( t ) { return ( 1 - t ) * ( 1 - t ) * ( 1 - t ); };
+        var bX = function( t ) { return b3( t ) * x1 + b2( t ) * x2 + b1( t ); };
+        var bY = function( t ) { return b3( t ) * y1 + b2( t ) * y2 + b1( t ); };
+        var dbX = function( t ) {
+
+            var v = 1 - t;
+            return 3 * ( 2 * ( t - 1 ) * t + v * v ) * x1 + 3 * ( -t * t * t + 2 * v * t ) * x2;
+        };
+
+        return function( t ) {
+
+            //Newton's method of finding x for current t (Doesn't always apply, so we need a fallback, but it's fast)
+            var x = t, t0, t1, x3, d2;
+            for( var t2 = x, i = 0; i < 8; i++ ) {
+
+                x3 = bX( t2 ) - x;
+                if( Math.abs( x3 ) < epsilon ) 
+                    return bY( t2 );
+
+                d2 = dbX( t2 );
+                if( Math.abs( d2 ) < 1e-6 ) 
+                    break;
+
+                t2 = t2 - x3 / d2;
+            }
+
+            //this is the fallback (bisection)
+            var t0 = 0, t1 = 1, t2 = x;
+            while( t0 < t1 ) {
+
+                x3 = bX( t2 );
+                if( Math.abs( x3 - x ) < epsilon ) 
+                    return bY( t2 );
+                if( x > x3 )
+                    t0 = t2;
+                else 
+                    t1 = t2;
+                t2 = ( t1 - t0 ) * .5 + t0;
+            } 
+
+            //couldn't find x for t
+            return bY( t );
+        };
+    }
 };
